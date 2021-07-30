5c2d4a21693d288356ed55980fd6d4ac87f3bce4
leonardo sifuentes <lsifuentes@avatar-global.com>
2021-07-30 12:51:59 -0500

    10

---
+++ E10/Indexed_collections.js
@@ -0,0 +1,35 @@
+//  Indexed collections
+
+// array collection
+
+const arr1 = new Array("le", "le", 1)
+const arr2 = ["le", "le", 2]
+
+//  Indexed
+arr1[3] = { nombre: "leonardo" }
+console.log(arr1[3]);
+
+
+// array property and functions and function callback
+//.length
+//.forEach
+//.push
+//.concat
+//.join
+//.pop
+//.shift
+//.unshift
+//.slice
+//.splice
+//.reverse
+//.sort
+//.indexOf
+//.lastIndexOf
+//.map
+//.filter
+//.every
+//.some
+//.reduce
+
+
+
