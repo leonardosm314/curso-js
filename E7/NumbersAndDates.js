0cc99c19a19e82efc652c8378b2d28df273827c9
leonardo sifuentes <lsifuentes@avatar-global.com>
2021-07-30 12:15:50 -0500

    7

---
+++ E7/NumbersAndDates.js
@@ -0,0 +1,15 @@
+//              Números binarios
+var FLT_SIGNBIT = 0b10000000000000000000000000000000; // 2147483648
+//              Números octales
+var n = 0755; // 493
+var m = 0644; // 420
+//              Números hexadecimales
+0xFFFFFFFFFFFFFFFFF // 295147905179352830000
+0x123456789ABCDEF   // 81985529216486900
+0XA                 // 10
+//              El objeto Number
+var biggestNum = Number.MAX_VALUE;
+var smallestNum = Number.MIN_VALUE;
+var infiniteNum = Number.POSITIVE_INFINITY;
+var negInfiniteNum = Number.NEGATIVE_INFINITY;
+var notANum = Number.NaN;
